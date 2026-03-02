d2d70cf9f42e55fca0f4ef387e719a90ad438b87
noahware <131519316+noahware@users.noreply.github.com>
2026-03-02 15:33:33 +0000

    Structured the loading of images and setup of stack

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,97 @@
 #include <portable_executable/file.hpp>
 #include <spdlog/spdlog.h>
 
+struct mapped_image_t
+{
+	emulator_t::address_type base_address;
+	emulator_t::size_type size;
+
+	emulator_t::address_type entry_point;
+};
+
+static void relocate_image(portable_executable::image_t* const image, const emulator_t::address_type runtime_base_address)
+{
+	const auto nt_headers = image->nt_headers();
+	const emulator_t::address_type delta = runtime_base_address - nt_headers->optional_header.image_base;
+
+	for (const auto [descriptor, virtual_address] : image->relocations())
+	{
+		if (descriptor.type == portable_executable::relocation_type_t::dir64)
+		{
+			const auto patch = reinterpret_cast<std::uint64_t*>(image->as<std::uint64_t>() + virtual_address + descriptor.offset);
+
+			*patch += delta;
+		}
+	}
+}
+
+static std::optional<mapped_image_t> map_kernel_image(emulator_t& emulator, const std::string_view name)
+{
+	portable_executable::file_t pe_file(name);
+
+	if (!pe_file.load())
+	{
+		spdlog::error("unable to load portable executable file");
+
+		return { };
+	}
+
+	const auto pe_image = pe_file.image();
+	const auto nt_headers = pe_image->nt_headers();
+
+	const auto image_size = nt_headers->optional_header.size_of_image;
+
+	const auto base_address = emulator.heap_allocate(image_size, prot_all);
+
+	if (!base_address)
+	{
+		spdlog::error("unable to map kernel memory");
+
+		return { };
+	}
+
+	const auto load_config = pe_image->load_config();
+
+	if (const auto security_cookie_absolute = load_config->security_cookie)
+	{
+		const auto security_cookie_rva = security_cookie_absolute - nt_headers->optional_header.image_base;
+
+		*reinterpret_cast<std::uint64_t*>(pe_image->as<std::uint64_t>() + security_cookie_rva) += *base_address;
+	}
+
+	relocate_image(pe_image, *base_address);
+
+	const auto image_start = pe_image->as<const std::uint8_t*>();
+	const std::span image_buffer(image_start, image_start + nt_headers->optional_header.size_of_image);
+
+	if (const auto error = emulator.write_virtual_memory(*base_address, image_buffer))
+	{
+		spdlog::error("unable to write kernel memory");
+
+		return { };
+	}
+
+	return mapped_image_t{
+		.base_address = *base_address, .size = image_size,
+		.entry_point = *base_address + nt_headers->optional_header.address_of_entry_point
+	};
+}
+
 static void set_up_stack(emulator_t& emulator)
 {
-	constexpr emulator_t::address_type stack_base_address = 0x10000;
 	constexpr emulator_t::size_type stack_size = 0x10000;
 
-	constexpr emulator_t::address_type starting_rsp_value = stack_base_address + stack_size - 0x1000;
+	const auto stack_base_address = emulator.heap_allocate(stack_size, prot_read_write);
 
-	emulator_err_t error = emulator.map_memory(stack_base_address, stack_size, prot_read_write);
+	emulator_err_t error = stack_base_address.error_or({});
 
 	error.throw_if("unable to map stack");
 
+	const emulator_t::address_type starting_rsp_value = *stack_base_address + stack_size - 0x1000;
+
 	emulator.write_register<x86::reg::rsp>(starting_rsp_value);
 
-	error = emulator.write_memory(starting_rsp_value, &emulator_t::thread_return_address, sizeof(emulator_t::thread_return_address));
+	error = emulator.write_virtual_memory(starting_rsp_value, &emulator_t::thread_return_address, sizeof(emulator_t::thread_return_address));
 
 	error.throw_if("unable to set return address");
 }
@@ -28,46 +105,31 @@ std::int32_t main()
 {
 	constexpr std::string_view pe_file_name = "test.bin";
 
-	portable_executable::file_t pe_file(pe_file_name);
-
-	if (!pe_file.load())
-	{
-		spdlog::error("unable to load portable executable file");
-
-		return 1;
-	}
-
-	const auto pe_image = pe_file.image();
-	const auto nt_headers = pe_image->nt_headers();
-
 	try
 	{
-		const auto emulator = std::static_pointer_cast<emulator_t>(std::make_shared<unicorn_emulator_t>());
+		const auto emulator = std::static_pointer_cast<emulator_t>(std::make_shared<hypermulator_t>());
 
 		set_up_stack(*emulator);
 
-		const emulator_t::address_type code_address = nt_headers->optional_header.image_base;
-		const emulator_t::address_type code_end_address = code_address + nt_headers->optional_header.size_of_image;
-		const emulator_t::address_type entry_point_address = code_address + nt_headers->optional_header.address_of_entry_point;
+		const auto pe_image = map_kernel_image(*emulator, pe_file_name);
 
-		const auto image_start = pe_image->as<const std::uint8_t*>();
-		const std::span image_buffer(image_start, image_start + nt_headers->optional_header.size_of_image);
+	    const emulator_t::address_type base_address = pe_image->base_address;
+	    const emulator_t::address_type end_address = base_address + pe_image->size;
+		const emulator_t::address_type entry_point_address = pe_image->entry_point;
 
-		emulator_err_t error = emulator->load_memory(code_address, image_buffer, prot_all);
+		spdlog::info("mapped image at 0x{:X}", base_address);
 
-		error.throw_if("memory loading");
-
-		error = emulator->hook_basic_block(
+		emulator_err_t error = emulator->hook_basic_block(
 			[emulator]()
 			{
 				const auto rip = emulator->read_register<x86::reg::rip, emulator_t::address_type>();
 
 				spdlog::info("basic block executed at 0x{:X}", rip);
 			},
-			code_address,
-			code_end_address
+			base_address,
+			end_address
 		).error_or({});
-
+		 
 		error.throw_if("basic block hook attach");
 
 		error = emulator->hook_invalid_memory(
@@ -75,7 +137,7 @@ std::int32_t main()
 			{
 				const auto rip = emulator->read_register<x86::reg::rip, emulator_t::address_type>();
 
-				spdlog::info("instruction at 0x{:X} access invalid memory address 0x{:X} with access type of {}", rip, faulting_address, static_cast<std::uint32_t>(access));
+				spdlog::info("instruction at 0x{:X} accessed invalid memory address 0x{:X} with access type of {}", rip, faulting_address, static_cast<std::uint32_t>(access));
 
 				return false;
 			},
@@ -89,8 +151,9 @@ std::int32_t main()
 		error = emulator->run_at(entry_point_address, emulator_t::thread_return_address);
 
 		const auto rip = emulator->read_register<x86::reg::rip, emulator_t::address_type>();
+		const auto rax = emulator->read_register<x86::reg::rax, emulator_t::address_type>();
 
-		spdlog::info("emulation finished at rip=0x{:X}", rip);
+		spdlog::info("emulation finished at rip=0x{:X}, rax=0x{:X}", rip, rax);
 
 		error.throw_if("emulation running");
 	}
