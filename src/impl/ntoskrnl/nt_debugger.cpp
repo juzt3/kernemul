bdf9c8df9852656a9fd8ead745ca23058ce14d81
noahware <131519316+noahware@users.noreply.github.com>
2026-03-17 16:29:04 +0000

    Added KdEnteredDebugger implementation

--- src/impl/ntoskrnl/nt_debugger.cpp
+++ src/impl/ntoskrnl/nt_debugger.cpp
@@ -8,18 +8,24 @@ void initialize_ntoskrnl_debugger_state(const std::shared_ptr<emulator_t>& emula
 	if (const auto addr = mapped_image.find_symbol("KdDebuggerNotPresent"))
 	{
 		constexpr std::uint8_t value = 1;
-		emulator_err_t error = emulator->write_virtual_memory(*addr, &value, sizeof(value));
-		error.throw_if("write KdDebuggerNotPresent");
 
-		spdlog::info("set KdDebuggerNotPresent (0x{:X}) to TRUE", *addr);
+		const emulator_err_t error = emulator->write_virtual_memory(*addr, &value, sizeof(value));
+		error.throw_if("write KdDebuggerNotPresent");
 	}
 
 	if (const auto addr = mapped_image.find_symbol("KdDebuggerEnabled"))
 	{
 		constexpr std::uint8_t value = 0;
-		emulator_err_t error = emulator->write_virtual_memory(*addr, &value, sizeof(value));
-		error.throw_if("write KdDebuggerEnabled");
 
-		spdlog::info("set KdDebuggerEnabled (0x{:X}) to FALSE", *addr);
+		const emulator_err_t error = emulator->write_virtual_memory(*addr, &value, sizeof(value));
+		error.throw_if("write KdDebuggerEnabled");
+	}
+
+	if (const auto addr = mapped_image.find_symbol("KdEnteredDebugger"))
+	{
+		constexpr std::uint8_t value = 0;
+
+		const emulator_err_t error = emulator->write_virtual_memory(*addr, &value, sizeof(value));
+		error.throw_if("write KdEnteredDebugger");
 	}
 }
