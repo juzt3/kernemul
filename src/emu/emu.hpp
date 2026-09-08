62f4644b9c02f274775502e07108cde2da0b149a
noahware <noahswtf@icloud.com>
2026-09-08 16:23:54 +0100

    Added basic skeleton for emu and vcpu

---
+++ src/emu/emu.hpp
@@ -0,0 +1,73 @@
+#pragma once
+#include <functional>
+#include <expected>
+#include <memory>
+#include <span>
+#include <vector>
+
+class status;
+class emu;
+struct emu_hook;
+
+enum mem_prot : std::uint8_t
+{
+	prot_none = 0,
+	prot_read = 1,
+	prot_write = 2,
+	prot_exec = 4,
+	prot_rw = prot_read | prot_write,
+	prot_rx = prot_read | prot_exec,
+	prot_wx = prot_write | prot_exec,
+	prot_all = prot_read | prot_write | prot_exec
+};
+
+using addr_t = std::uint64_t;
+
+class vcpu
+{
+public:
+	explicit vcpu(emu* const emu)
+		:	emu_(emu) { }
+
+	virtual status run() = 0;
+
+protected:
+	emu* emu_;
+};
+
+class emu
+{
+public:
+	using hook_handle = emu_hook*;
+	using mem_hk_cb = std::function<void(vcpu&, addr_t, std::size_t, mem_prot)>;
+
+	virtual hook_handle hook_mem(addr_t start_addr, addr_t end_addr, mem_prot prot, mem_hk_cb) = 0;
+	virtual void remove_hook(hook_handle handle) = 0;
+
+	virtual void map_mem(addr_t addr, std::size_t size, mem_prot prot) = 0;
+	virtual void unmap_mem(addr_t addr, std::size_t size, mem_prot prot) = 0;
+	virtual void read_mem(addr_t addr, void* buf, std::size_t size) = 0;
+	virtual void write_mem(addr_t addr, const void* buf, std::size_t size) = 0;
+
+	template <class T, class... Args>
+		requires std::derived_from<T, vcpu>
+	std::shared_ptr<T> create_vcpu(Args&&... args)
+	{
+		auto core = std::make_shared<T>(this, std::forward<Args>(args)...);
+		add_vcpu(core);
+		return core;
+	}
+
+	void add_vcpu(std::shared_ptr<vcpu> cpu)
+	{
+		cpus_.push_back(std::move(cpu));
+	}
+
+	[[nodiscard]] std::span<const std::shared_ptr<vcpu>> cpus() const noexcept
+	{
+		return cpus_;
+	}
+
+protected:
+	std::vector<std::shared_ptr<vcpu>> cpus_;
+};
