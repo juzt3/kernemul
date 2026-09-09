d0d6b18cb732e09323050e63a5039fa17e80231b
noahware <noahswtf@icloud.com>
2026-09-09 19:55:27 +0100

    Added user process creation + process ids

--- src/kernel/kernel.hpp
+++ src/kernel/kernel.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include "../process/process.hpp"
 #include "../emu/emu.hpp"
+#include <map>
+#include <mutex>
 
 class os_emulator
 {
@@ -13,12 +15,52 @@ public:
 	emu& emu() { return *emu_; }
 
 protected:
-	std::shared_ptr<struct emu> emu_;
+	std::shared_ptr<class emu> emu_;
 };
 
-struct win_kernel_state
+struct kernel_state
 {
-	kernel_process sys_proc;
+	virtual ~kernel_state() = default;
+
+	virtual std::shared_ptr<process> create_user_process(std::string_view name) = 0;
+
+	std::shared_ptr<process> find_process(process::id_type id)
+	{
+		std::lock_guard lock(proc_mtx_);
+		const auto it = processes.find(id);
+		return it != processes.end() ? it->second : nullptr;
+	}
+
+protected:
+	std::mutex proc_mtx_;
+	std::map<process::id_type, std::shared_ptr<process>> processes;
+};
+
+struct win_kernel_state : kernel_state
+{
+	static constexpr process::id_type sys_proc_id = 4;
+	static constexpr process::id_type proc_id_step = 4;
+
+	std::shared_ptr<kernel_process> sys_proc;
+
+	win_kernel_state()
+		:	sys_proc(std::make_shared<kernel_process>(sys_proc_id))
+	{
+		processes[sys_proc_id] = sys_proc;
+	}
+
+	std::shared_ptr<process> create_user_process(std::string_view name) override
+	{
+		std::lock_guard lock(proc_mtx_);
+		const auto id = next_id_;
+		auto proc = std::make_shared<user_process>(id);
+		processes[id] = proc;
+		next_id_ += proc_id_step;
+		return proc;
+	}
+
+private:
+	process::id_type next_id_ = 8;
 };
 
 class windows_emulator : public os_emulator
--- src/process/process.hpp
+++ src/process/process.hpp
@@ -29,27 +29,35 @@ struct proc_module
 class process
 {
 public:
-	virtual ~process() = default;
+	using id_type = std::uint32_t;
 
+	explicit process(const id_type id)
+		:	id_(id) { }
+
+	virtual ~process() = default;
 	virtual void module_add_cb([[maybe_unused]] proc_module& mod) { }
 
 	std::shared_ptr<proc_module> add_module(std::string_view name, addr_t addr, const pe::image* pe);
 	[[nodiscard]] std::shared_ptr<proc_module> find_module(std::string_view name) const;
 
 	[[nodiscard]] std::shared_ptr<addr_space> addr_space() const;
+	[[nodiscard]] id_type id() const { return id_; }
 
 protected:
+	id_type id_;
 	std::unordered_map<std::string_view, std::shared_ptr<proc_module>> modules_;
 	std::shared_ptr<struct addr_space> addr_space_;
 };
 
 class user_process : public process
 {
-
+public:
+	using process::process;
 };
 
 class kernel_process : public process
 {
 public:
+	using process::process;
 	void module_add_cb(proc_module& mod) override;
 };
