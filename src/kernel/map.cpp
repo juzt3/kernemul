2a067f6f5f7d4b5f270939be937380b030fc8609
noahware <noahswtf@icloud.com>
2026-09-09 19:21:19 +0100

    Added applying relocations to map_img

--- src/kernel/map.cpp
+++ src/kernel/map.cpp
@@ -1,6 +1,7 @@
 #include "map.hpp"
 #include "../emu/addr_space.hpp"
 #include "../process/process.hpp"
+#include "../util/log.hpp"
 
 bool krnl::map_img(process& proc, const pe::image* const img, const bool supervisor)
 {
@@ -18,6 +19,8 @@ bool krnl::map_img(process& proc, const pe::image* const img, const bool supervi
 		return false;
 	}
 
+	space->write_mem(addr, img->as(), img->size());
+
 	for (const auto sec : img->sections())
 	{
 		auto sec_flags = flags;
@@ -30,7 +33,40 @@ bool krnl::map_img(process& proc, const pe::image* const img, const bool supervi
 		space->prot_mem(addr + sec.virtual_address, sec.virtual_size, sec_flags);
 	}
 
-	space->write_mem(addr, img->as(), img->size());
+	for (const auto imp : img->imports())
+	{
+		const auto mod = proc.find_module(imp.module_name);
+
+		if (!mod)
+		{
+			LOG_ERR("unable to find import module {}", imp.module_name);
+			return false;
+		}
+
+		const auto patch_loc = addr + imp.iat_slot.rva();
+		const auto import_addr = mod->find_export(imp.import_name);
+
+		if (!import_addr)
+		{
+			LOG_ERR("unable to find import {}!{}", imp.module_name, imp.import_name);
+			return false;
+		}
+
+		space->write_mem(patch_loc, import_addr.value());
+	}
+
+	const std::uintptr_t delta = reinterpret_cast<std::uintptr_t>(addr) - img->base_addr();
+
+	for (const auto reloc : img->relocs())
+	{
+		if (reloc.type != pe::reloc_type::dir64)
+			continue;
+
+		const auto reloc_addr = addr + reloc.loc.rva();
+		const auto val = space->read_mem<std::uint64_t>(reloc_addr);
+
+		space->write_mem(reloc_addr, val + delta);
+	}
 
 	return true;
 }
