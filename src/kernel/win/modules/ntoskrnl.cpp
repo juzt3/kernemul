8f22de8cae4617d0af7533617328fe012b2e32ec
noahware <noahswtf@icloud.com>
2026-09-10 20:06:02 +0100

    Added string formatting

--- src/kernel/win/modules/ntoskrnl.cpp
+++ src/kernel/win/modules/ntoskrnl.cpp
@@ -1,12 +1,21 @@
 #include "ntoskrnl.hpp"
 #include "../../kernel.hpp"
+#include "../../../util/format.hpp"
 
 void modules::register_ntoskrnl(kernel_state& state, proc_module& mod)
 {
 	state.redirect(mod, "DbgPrintEx",
 		[](vcpu& cpu, std::uint32_t component_id, std::uint32_t level, std::string format) -> std::uint32_t
 		{
-			LOG_INFO("DbgPrintEx called (component={}, level={}) : {}", component_id, level, format);
+			std::size_t arg_idx = 3;
+			auto& space = *cpu.curr_addr_space();
+			const auto& conv = *cpu.emu_->call_conv();
+			const auto msg = guest::vsprintf(space, format, [&]() -> std::uint64_t
+			{
+				return conv.arg<std::uint64_t>(cpu, arg_idx++);
+			});
+
+			LOG_INFO("DbgPrintEx (component={}, level={}) : {}", component_id, level, msg);
 			return 0;
 		});
 }
