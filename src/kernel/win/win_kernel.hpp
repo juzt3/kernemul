812f8d7d4e8a8b605860418c6ab41a775ef18618
noahware <noahswtf@icloud.com>
2026-09-10 08:34:35 +0100

    Call kernel state init in windows_emulator constructor

--- src/kernel/win/win_kernel.hpp
+++ src/kernel/win/win_kernel.hpp
@@ -41,10 +41,16 @@ private:
 class windows_emulator : public os_emulator
 {
 public:
-	using os_emulator::os_emulator;
+	explicit windows_emulator(std::shared_ptr<class emu> emu)
+		: os_emulator(std::move(emu))
+	{
+		kernel_space_ = emu_->mem()->create_addr_space();
+		kernel_.init(*kernel_space_);
+	}
 
 	win_kernel_state& kernel() { return kernel_; }
 
 private:
+	std::shared_ptr<addr_space> kernel_space_;
 	win_kernel_state kernel_;
 };
