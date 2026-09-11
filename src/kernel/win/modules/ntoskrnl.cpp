d29fc78407e15457b538038cb6b391e4c8f617f2
noahware <noahswtf@icloud.com>
2026-09-11 15:18:26 +0100

    Made DbgPrint handler

--- src/kernel/win/modules/ntoskrnl.cpp
+++ src/kernel/win/modules/ntoskrnl.cpp
@@ -4,6 +4,21 @@
 
 void modules::register_ntoskrnl(kernel_state& state, proc_module& mod)
 {
+	state.redirect(mod, "DbgPrint",
+		[](vcpu& cpu, std::string format) -> std::uint32_t
+		{
+			std::size_t arg_idx = 1;
+			auto& space = *cpu.curr_addr_space();
+			const auto& conv = *cpu.emu()->call_conv();
+			const auto msg = guest::vsprintf(space, format, [&]() -> std::uint64_t
+			{
+				return conv.arg<std::uint64_t>(cpu, arg_idx++);
+			});
+
+			LOG_INFO("DbgPrint: {}", msg);
+			return 0;
+		});
+
 	state.redirect(mod, "DbgPrintEx",
 		[](vcpu& cpu, std::uint32_t component_id, std::uint32_t level, std::string format) -> std::uint32_t
 		{
